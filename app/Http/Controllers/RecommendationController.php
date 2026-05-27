b79613856e75b2ec3a27365baa49db56b3d73142
CalvinEmanuel <calvinkyura@gmail.com>
2026-05-27 22:15:17 +0700

    commit dari calvin

--- app/Http/Controllers/RecommendationController.php
+++ app/Http/Controllers/RecommendationController.php
@@ -14,17 +14,17 @@ class RecommendationController extends Controller
             (object) [
                 'song_title' => 'Kasih Aba Aba',
                 'artist' => 'Tenxi',
-                'reason' => 'Beat-nya asyik dan bikin semangat, cocok banget masuk daftar rekomendasi utama.'
+                'reason' => 'Beat-nya asik dan bikin nagih banget, cocok masuk daftar rekomendasi utama.'
             ],
             (object) [
                 'song_title' => 'Malu Malu',
                 'artist' => 'Indahkus',
-                'reason' => 'Vibe lagunya ceria dan santai, pas buat didengerin sambil rehat santai.'
+                'reason' => 'Vibes lagunya ceria dan santai, pas banget buat didengerin kalo lagi santai.'
             ],
             (object) [
                 'song_title' => 'Dirimu Yang Dulu',
                 'artist' => 'Anggis Devaki',
-                'reason' => 'Lagu dengan lirik mendalam dan aransemen yang enak banget di telinga.'
+                'reason' => 'Lagu dengan sadvibes yang berasa banget dihati.'
             ]
         ];
         
