b3181efd25a03d79f1a7064fe54d0e48effdeb1d
yensenlemuel4 <yensenlemuel4@gmail.com>
2026-05-29 09:26:15 +0700

    Menambah History View dan Get

--- app/Http/Controllers/HistoryController.php
+++ app/Http/Controllers/HistoryController.php
@@ -2,17 +2,30 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Recommendation;
+use App\Models\History;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class HistoryController extends Controller
-{
+{   // Kode di comment and taruh [PLACEHOLDER] sementara
+    // 1.Menampilkan History
     public function index()
     {
-        // Untuk menampilkan riwayat rekomendasi lagu user
-        // Belum ada fitur play jadi kosong untuk sementara
+        // $history = [PLACEHOLDER];
+        // return view('history.index', compact('history'));
+    }
+    public function store(Request $request)
+    {   // 2. Menyimpan History
+        // $request->validate([
+        //     ['[PLACEHOLDER]' => 'required|exists:[PLACEHOLDER]']  
+        // ]);
 
+        // History::create([
+        //     '[PLACEHOLDER]' => Auth::id(),
+        //     '[PLACEHOLDER]' => $request->[PLACEHOLDER],
+        // ]);
 
+        // return redirect()->back()->with('Lagu diputar!');
     }
 }
+
---
+++ app/Models/History.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Recommendation extends Model
+{
+        protected $fillable = [
+        'user_id',
+        'song_title',
+        'artist',
+    ];
+}
