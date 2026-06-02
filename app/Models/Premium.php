3184ac863adae8a24cb08f95cb8c33ceb70c04d8
CalvinEmanuel <calvinkyura@gmail.com>
2026-06-02 19:37:39 +0700

    Membuat model Premium dan menambahkan relasi untuk ke user

--- app/Models/Premium.php
+++ app/Models/Premium.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Premium extends Model
 {
-    //
+    // Mendaftarkan kolom supaya aman disaat nanti diinput lewat form 
+    protected $fillable = [
+        'user_id',
+        'package_name',
+        'status',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
