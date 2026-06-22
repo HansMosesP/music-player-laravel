deb41ecabd73cfc308cfa2fd4c5a4f846b9dd5ff
yensenlemuel4 <yensenlemuel4@gmail.com>
2026-06-22 12:00:05 +0700

    Perbaikan Model History

--- app/Models/History.php
+++ app/Models/History.php
@@ -1,14 +1,26 @@
+app/Models/History.php
+ 
 <?php
-
+ 
 namespace App\Models;
-
+ 
 use Illuminate\Database\Eloquent\Model;
-
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+ 
 class History extends Model
 {
-        protected $fillable = [
+    protected $fillable = [
         'user_id',
-        'song_title',
-        'artist',
+        'song_id',
     ];
+ 
+    public function song(): BelongsTo
+    {
+        return $this->belongsTo(Song::class);
+    }
+ 
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
