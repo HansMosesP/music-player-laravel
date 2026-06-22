68f1f16d0692cd8919b5fed0878029912833b9f1
HansMosesP <mosesdrivefto@gmail.com>
2026-06-22 11:59:37 +0700

    Otomatis Create Lagu Pas Migrate

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,24 +2,66 @@
 
 namespace Database\Seeders;
 
+use App\Models\Song;
 use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
-    use WithoutModelEvents;
-
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
-        // User::factory(10)->create();
+        User::firstOrCreate(
+            [
+                'email' => 'admin@example.com',
+            ],
+            [
+                'name' => 'Admin',
+                'password' => Hash::make('password'),
+            ]
+        );
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        $songs = [
+            [
+                'title' => 'Night Changes',
+                'artist' => 'One Direction',
+                'album' => 'Four',
+                'views' => 0,
+            ],
+            [
+                'title' => 'Perfect',
+                'artist' => 'Ed Sheeran',
+                'album' => 'Divide',
+                'views' => 0,
+            ],
+            [
+                'title' => 'Faded',
+                'artist' => 'Alan Walker',
+                'album' => 'Different World',
+                'views' => 0,
+            ],
+            [
+                'title' => 'Ku Mau Dia',
+                'artist' => 'Andmesh',
+                'album' => 'Cinta Luar Biasa',
+                'views' => 0,
+            ],
+            [
+                'title' => 'Happier',
+                'artist' => 'Marshmello ft. Bastille',
+                'album' => 'Happier',
+                'views' => 0,
+            ],
+        ];
+
+        foreach ($songs as $song) {
+            Song::firstOrCreate(
+                [
+                    'title' => $song['title'],
+                    'artist' => $song['artist'],
+                ],
+                $song
+            );
+        }
     }
 }
